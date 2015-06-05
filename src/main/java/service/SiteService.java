3b8c5f237ed15b3a944d4b523e808d2ded12d09e
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2015-06-05 17:53:15 +0300

    moved onto annotation config

--- src/main/java/service/SiteService.java
+++ src/main/java/service/SiteService.java
@@ -1,4 +1,7 @@
 package service;
 
+import entity.User;
+
 public interface SiteService {
+    User getUserByName(String input);
 }
--- src/main/java/service/SiteServiceImpl.java
+++ src/main/java/service/SiteServiceImpl.java
@@ -1,6 +1,18 @@
 package service;
 
+import dao.PageDAO;
+import dao.PermissionDAO;
+import dao.UserDAO;
+import entity.User;
+
 public class SiteServiceImpl implements SiteService {
 
+    private PageDAO pageDAO;
+    private PermissionDAO permissionDAO;
+    private UserDAO userDAO;
 
+
+    public User getUserByName(String input) {
+        return null;
+    }
 }
